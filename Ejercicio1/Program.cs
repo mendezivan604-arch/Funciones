cb3608555e6306b9a9e24983a0bb17854c7cf8e9
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 19:20:46 -0600

    Ejercicio 1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿double Suma(double a, double b)
+{
+    return a + b;
+}
+void numero()
+{
+    Console.WriteLine("Ingrese un numero: ");
+}
+
+numero();
+double num1 = double.Parse(Console.ReadLine());
+numero();
+double num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("La suma es: " + Suma(num1, num2));
+
+
