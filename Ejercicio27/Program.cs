a39697544f3c3f13577912dc68b819bb6747fef4
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:42:29 -0600

    Ejercicio 27

---
+++ Ejercicio27/Program.cs
@@ -0,0 +1,12 @@
+﻿void PrecioConDescuento()
+{
+    Console.WriteLine("Ingrese el precio del producto: Q");
+    double precio = double.Parse(Console.ReadLine());
+
+    double descuento = precio * 0.10;
+    double precioFinal = precio - descuento;
+
+    Console.WriteLine("El precio con descuento del 10% es: Q" + precioFinal);
+}
+
+PrecioConDescuento();
