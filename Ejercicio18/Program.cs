ecae58d76a1df9df132e50c2c5c0efa20f235d4b
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:07:04 -0600

    Ejercicio 18

---
+++ Ejercicio18/Program.cs
@@ -0,0 +1,16 @@
+﻿double TotalFactura(double cantidadProductos)
+{
+    double totalproductos = 0;
+    for (int i = 0; i < cantidadProductos; i++)
+    {
+        Console.WriteLine("Ingrese el precio del producto " + (i + 1) + ": Q");
+        double precio = double.Parse(Console.ReadLine());
+        totalproductos += precio;
+    }
+
+    return totalproductos;
+}
+Console.WriteLine("Ingrese cantidad de productos: ");
+double cantidadProductos = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Total Factura: Q" + TotalFactura(cantidadProductos));
