a50a25f407fa6ece2e5b3950e03f62ae978f3652
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:34:46 -0600

    Ejercicio 25

---
+++ Ejercicio25/Program.cs
@@ -0,0 +1,14 @@
+﻿void AreaRectangulo()
+{
+    Console.WriteLine("Ingrese la base del rectángulo: ");
+    double baseRectangulo = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la altura del rectángulo: ");
+    double altura = double.Parse(Console.ReadLine());
+
+    double area = baseRectangulo * altura;
+
+    Console.WriteLine("El área del rectángulo es: " + area);
+}
+
+AreaRectangulo();
