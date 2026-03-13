7d834e355ca6fc2098ae40dbd21538712f762fdf
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:04:39 -0600

    Ejercicio 17

---
+++ Ejercicio17/Program.cs
@@ -0,0 +1,18 @@
+﻿double PromedioGrupo(int cantidadEstudiantes)
+{
+    double sumaNotas = 0;
+
+    for (int i = 0; i < cantidadEstudiantes; i++)
+    {
+        Console.WriteLine("Ingrese la nota del estudiante " + (i + 1) + ": ");
+        double nota = double.Parse(Console.ReadLine());
+        sumaNotas += nota;
+    }
+
+    return sumaNotas / cantidadEstudiantes;
+}
+
+Console.WriteLine("Ingrese la cantidad de estudiantes: ");
+int cantidadEstudiantes = int.Parse(Console.ReadLine());
+
+Console.WriteLine("El promedio del grupo es: " + PromedioGrupo(cantidadEstudiantes));
