48c2bb451bd31cd7d5fa0c913ad4cefc6a9f1b8c
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 20:33:42 -0600

    Ejercicio 5

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,11 @@
+﻿double AreaRectangulo(double vase, double altura)
+{
+    return vase * altura;
+}
+
+Console.WriteLine("Ingrese base del rectángulo: ");
+double vase = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese altura del rectángulo: ");
+double altura = double.Parse(Console.ReadLine());
+
+Console.WriteLine("El área del rectángulo es: " + AreaRectangulo(vase, altura));
