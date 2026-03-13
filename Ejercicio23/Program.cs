85dc5b542d214639c865b905e2b986d0e8728677
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:28:30 -0600

    Ejercicio 23

---
+++ Ejercicio23/Program.cs
@@ -0,0 +1,10 @@
+﻿void ingnumero()
+{
+    Console.WriteLine("Ingrese un numero: ");
+}
+ingnumero();
+int numero = int.Parse(Console.ReadLine());
+ingnumero();
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("La suma es: " + (numero + numero2));
