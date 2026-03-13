45303c2127ed8f62be5ec6441c18891b6dc62266
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:46:44 -0600

    Ejercicio 29

---
+++ Ejercicio29/Program.cs
@@ -0,0 +1,9 @@
+﻿void CantidadEstudiantes()
+{
+    Console.WriteLine("Ingrese el número de estudiantes del curso: ");
+    int estudiantes = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("En la clase hay " + estudiantes + " estudiantes.");
+}
+
+CantidadEstudiantes();
