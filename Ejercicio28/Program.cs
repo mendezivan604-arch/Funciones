8d809abd587b460625515909f454c931500a1c8f
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:45:06 -0600

    Ejercicio 28

---
+++ Ejercicio28/Program.cs
@@ -0,0 +1,17 @@
+﻿void PromedioEstudiante()
+{
+    Console.WriteLine("Ingrese la primera calificación: ");
+    double nota1 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la segunda calificación: ");
+    double nota2 = double.Parse(Console.ReadLine());
+
+    Console.WriteLine("Ingrese la tercera calificación: ");
+    double nota3 = double.Parse(Console.ReadLine());
+
+    double promedio = (nota1 + nota2 + nota3) / 3;
+
+    Console.WriteLine("El promedio del estudiante es: " + promedio);
+}
+
+PromedioEstudiante();
