5ab4df252011845230e12bab914db35f6ca28c83
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 20:52:07 -0600

    Ejercicio 7

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,10 @@
+﻿double PrecioFinal(double precio, double descuento)
+{
+    return precio - (precio * (descuento / 100));
+}
+Console.WriteLine("Ingrese el precio: ");
+double precio = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el descuento: %");
+double descuento = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Precio final: " + PrecioFinal(precio, descuento));
