0fced1b1742ba24e58783392c7c847e45527daa2
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 20:56:45 -0600

    Ejercicio 8

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,18 @@
+﻿double Promedio(double num1, double num2, double num3)
+{
+    return (num1 + num2 + num3) / 3;
+}
+void numero()
+{
+    Console.WriteLine("Ingrese un numero: ");
+}
+
+numero();
+double num1 = double.Parse(Console.ReadLine());
+numero();
+double num2 = double.Parse(Console.ReadLine());
+numero();
+double num3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Promedio: " + Promedio(num1, num2, num3));
+
