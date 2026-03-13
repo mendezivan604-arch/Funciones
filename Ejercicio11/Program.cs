89e219fafc4947e11cadf18ba88a694522ecd446
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 21:10:35 -0600

    Ejercicio 11

---
+++ Ejercicio11/Program.cs
@@ -0,0 +1,14 @@
+﻿double Libras(double kg)
+{
+    return kg * 2.20462;
+}
+
+void ingkg()
+{
+    Console.WriteLine("Ingrese el peso en kg: ");
+}
+
+ingkg();
+double kg = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Libras: " + Libras(kg));
