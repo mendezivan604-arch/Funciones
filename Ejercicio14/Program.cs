c1c729d20b7a838c236412ad83150b69454ed53f
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 21:48:44 -0600

    Ejercicio 14

---
+++ Ejercicio14/Program.cs
@@ -0,0 +1,10 @@
+﻿double Velocidad(double distancia, double tiempo)
+{
+    return distancia / tiempo;
+}
+Console.WriteLine("Ingrese distancia recorrida: ");
+double distancia = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese tiempo empleado: ");
+double tiempo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Velocidad: " + Velocidad(distancia, tiempo));
