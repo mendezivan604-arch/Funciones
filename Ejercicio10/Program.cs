aad0561540c083e9ee3bb66e7b67d2bcb74c1c74
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 21:05:56 -0600

    Ejercicio 10

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,13 @@
+﻿int Cubo(int a)
+{
+    return a * a * a;
+}
+
+void ingnumero()
+{
+    Console.WriteLine("Ingrese un numero: ");
+}
+ingnumero();
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("El cubo es: " + Cubo(numero));
