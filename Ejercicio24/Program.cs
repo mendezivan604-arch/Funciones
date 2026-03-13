b1c18109a292d5c25d5ff018f81d502bfb2b8e76
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:32:41 -0600

    Ejercicio 24

---
+++ Ejercicio24/Program.cs
@@ -0,0 +1,11 @@
+﻿void AreaCirculo()
+{
+    Console.WriteLine("Ingrese el radio del círculo: ");
+    double radio = double.Parse(Console.ReadLine());
+
+    double area = 3.1416 * (radio * radio);
+
+    Console.WriteLine("El área del círculo es: " + area);
+}
+
+AreaCirculo();
