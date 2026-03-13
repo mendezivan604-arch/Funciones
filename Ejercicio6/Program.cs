e0673b3f47495514210b524ee516ee73327c48e1
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 20:42:13 -0600

    Ejercicio 6

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,13 @@
+﻿double Farenheit(double celsius)
+{
+    return (celsius * 9 / 5) + 32;
+}
+ void ingcelsius()
+{
+    Console.WriteLine("Ingrese la temperatura en grados Celsius: ");
+}
+
+ingcelsius();
+double celsius = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Farenheit: " + Farenheit(celsius));
