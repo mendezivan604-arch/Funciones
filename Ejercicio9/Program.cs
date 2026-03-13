7c55b4a9a3bb4b145eb7af7ddf4acb810c395cbb
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 21:01:15 -0600

    Ejercicio 9

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,11 @@
+﻿double SalarioTotal(double salario, double horas)
+{
+    return salario * horas;
+}
+
+Console.WriteLine("Ingrese el salario por hora: Q");
+double salario = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese las horas trabajadas: ");
+double horas = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Salario total: Q" + SalarioTotal(salario, horas));
