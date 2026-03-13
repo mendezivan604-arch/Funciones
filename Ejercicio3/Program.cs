36ca80666af7fa92342114fff2efe03094f4d659
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 19:29:57 -0600

    Ejercicio 3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,13 @@
+﻿int Doble (int a)
+{
+    return a * 2;
+}
+void ingnumero()
+{
+    Console.WriteLine("Ingrese un numero: ");
+}
+
+ingnumero();
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("El doble es: " + Doble(numero));
