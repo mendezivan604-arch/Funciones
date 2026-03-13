bc1f62ca8cf0359c9a2daa40c6a6d5b08419dfd2
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 21:59:17 -0600

    Ejercicio 16

---
+++ Ejercicio16/Program.cs
@@ -0,0 +1,8 @@
+﻿double Horas(double minutos)
+{
+    return minutos / 60;
+}
+Console.WriteLine("Ingrese minutos: ");
+double minutos = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Horas: " + Horas(minutos));
