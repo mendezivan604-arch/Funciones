ebfcda672e365735610cbcaa18f869faa064cfbf
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:12:44 -0600

    Ejercicio 19

---
+++ Ejercicio19/Program.cs
@@ -0,0 +1,18 @@
+﻿double PromedioFinal(string nombre, double nota1, double nota2, double nota3)
+{
+    return (nota1 + nota2 + nota3) / 3;
+}
+
+Console.WriteLine("Ingrese el nombre del estudiante: ");
+string nombre = Console.ReadLine();
+
+Console.WriteLine("Ingrese la primera calificación: ");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la segunda calificación: ");
+double nota2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la tercera calificación: ");
+double nota3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("El promedio final de " + nombre + " es: " + PromedioFinal(nombre, nota1, nota2, nota3));
