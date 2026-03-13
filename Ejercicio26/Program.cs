72c239d05af915d22d34b6b4f1cc832452bc308c
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 22:39:29 -0600

    Ejercicio 26

---
+++ Ejercicio26/Program.cs
@@ -0,0 +1,12 @@
+﻿void AñoNacimiento()
+{
+    Console.WriteLine("Ingrese su edad: ");
+    int edad = int.Parse(Console.ReadLine());
+
+    int añoActual = 2026;
+    int añoNacimiento = añoActual - edad;
+
+    Console.WriteLine("Su año aproximado de nacimiento es: " + añoNacimiento);
+}
+
+AñoNacimiento();
