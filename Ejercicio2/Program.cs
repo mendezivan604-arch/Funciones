85190b6f344be739195c9c97aee5f0fef48f680d
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 19:24:20 -0600

    Ejercicio 2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿int Cuadrado(int a)
+{
+    return a * a;
+}
+void ingnumero()
+{
+    Console.WriteLine("Ingrese un numero: ");
+}
+
+ingnumero();
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine("El cuadrado es: " + Cuadrado(numero));
