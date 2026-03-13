5c4838e8074743b2e64001e67de6be87b2ad516d
Ivan mendez <mendezivan604@gmail.com>
2026-03-12 21:25:41 -0600

    Ejercicio 12

---
+++ Ejercicio12/Program.cs
@@ -0,0 +1,9 @@
+﻿int Mensaje(int edad)
+{
+    Console.WriteLine("Tiene " + edad + " años");
+    return edad;
+}
+
+Console.WriteLine("Ingrese su edad: ");
+int edad = int.Parse(Console.ReadLine());
+Mensaje(edad);
